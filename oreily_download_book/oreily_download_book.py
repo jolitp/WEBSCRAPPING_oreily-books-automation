3bb450c59817ef6ca4058240eec528417f4f7ad8
João Luís <jolitp@gmail.com>
2021-07-12 14:34:30 -0300

    refactor book download

--- oreily_download_book/oreily_download_book.py
+++ oreily_download_book/oreily_download_book.py
@@ -13,49 +13,49 @@ from icecream import ic
 
 
 # setup driver
-chromedriver_path = Path("/home/jolitp/Applications/chromedriver")
-driver = webdriver.Chrome(chromedriver_path)
-driver.maximize_window()
+CHROME_DRIVER_PATH = Path("/home/jolitp/Applications/chromedriver")
+DRIVER = webdriver.Chrome(CHROME_DRIVER_PATH)
+DRIVER.maximize_window()
 
 # login information
 EMAIL = "libed17686@advew.com"
 PASSWORD = "h3dg3h0g"
 
-# def login_oreilly():
+def login_oreilly():
+    # open site
+    DRIVER.get("https://learning.oreilly.com/")
+    # get email filed element and insert email
+    email_field = DRIVER.find_element_by_xpath("//input[@name='email']")
+    email_field.clear()
+    email_field.send_keys(EMAIL)
+    time.sleep(.1)
 
-#     ...
+    # get password field element and insert password
+    password_field = DRIVER.find_element_by_xpath("//input[@name='password']")
+    password_field.clear()
+    password_field.send_keys(PASSWORD)
 
-# def main():
+    # wait for sign in button to be clickable
+    time.sleep(.3)
+    password_field.send_keys(Keys.ENTER)
 
-# 	...
+def go_to_book_page(url: str):
+    time.sleep(3)
+    DRIVER.get(url)
+    ...
 
-# if __name__ == '__main__':
-#     main()
+def main():
+    login_oreilly()
+    url = "https://learning.oreilly.com/library/view/fluent-python-2nd/9781492056348/"
+    go_to_book_page(url)
+    ...
 
-# open site
-# driver.get("https://learning.oreilly.com/library/view/fluent-python-2nd/9781492056348/")
-driver.get("https://learning.oreilly.com/")
-# get email filed element and insert email
-email_field = driver.find_element_by_xpath("//input[@name='email']")
-email_field.clear()
-email_field.send_keys(EMAIL)
-time.sleep(.1)
-
-# get password field element and insert password
-password_field = driver.find_element_by_xpath("//input[@name='password']")
-password_field.clear()
-password_field.send_keys(PASSWORD)
-time.sleep(.3)
-password_field.send_keys(Keys.ENTER)
+if __name__ == '__main__':
+    main()
 
 
 
 
-
-
-
-time.sleep(3)
-driver.get("https://learning.oreilly.com/library/view/fluent-python-2nd/9781492056348/")
 
 
 
