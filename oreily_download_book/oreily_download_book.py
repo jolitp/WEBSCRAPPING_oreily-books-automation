4d37dfbd45603eed26db287270bcb1314558ff70
João Luís <jolitp@gmail.com>
2021-07-14 14:28:22 -0300

    adds downloading cover page img

--- oreily_download_book/oreily_download_book.py
+++ oreily_download_book/oreily_download_book.py
@@ -2,16 +2,21 @@
 
 # spell-checker: word jolitp pyautogui lxml
 
+# region imports
 from pathlib import Path
 import time
+import datetime
+from datetime import date
 import os
 import re
-import bs4
+from dataclasses import dataclass, field
+import subprocess
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.remote.webelement import WebElement
+import selenium.webdriver.support.ui as ui
 
 from rich.console import Console
 from rich.traceback import install as rich_traceback_install
@@ -21,10 +26,82 @@ from pyautogui import Point
 
 from bs4 import BeautifulSoup
 
-from lxml import etree
+from natsort import natsorted, ns
 
 from icecream import ic
+# endregion imports
 
+
+# region classes
+
+@dataclass
+class Chapter:
+    """Class for keeping track of an item in inventory."""
+    title: str = ""
+    sections: list = field(default_factory=list)
+
+    def __str__(self) -> str:
+        result = f"[#00AAAA]title[/]: [#FF8800]{self.title}[/]\n"
+        result += "[#00AAAA]sections[/] : [\n"
+        for section in self.sections:
+            result += f"    [#FF8800]{section}[/]\n"
+        result += "]\n"
+        return result
+
+    def print(self):
+        string_repr = self.__str__()
+        CONSOLE.print(string_repr)
+        ...
+
+
+@dataclass
+class Book:
+    """Class for keeping track of an item in inventory."""
+    title: str = ""
+    author: str = ""
+    time_to_complete: time = None
+    topics: str = ""
+    publisher: str = ""
+    # publisher_info: str = ""
+    # resources_section: str = ""
+    publication_date: date = None
+    is_early_release: bool = None
+    # stars: int = ""
+    description: str = ""
+    # reviews: list = field(default_factory=list)
+    chapters: list = field(default_factory=list)
+
+    def __str__(self) -> str:
+        result = ""
+
+        result += f"[#00AAAA]title[/]: [#FF8800]{self.title}[/]\n"
+        result += f"[#00AAAA]author[/]: [#FF8800]{self.author}[/]\n"
+        result += f"[#00AAAA]publisher[/]: [#FF8800]{self.publisher}[/]\n"
+        result += f"[#00AAAA]publication date[/]: [#FF8800]{self.publication_date}[/]\n"
+        result += f"[#00AAAA]topics[/]: [#FF8800]{self.topics}[/]\n"
+        result += f"[#00AAAA]time to complete[/]: [#FF8800]{self.time_to_complete}[/]\n"
+        result += f"[#00AAAA]description[/]: [#FF8800]{self.description}[/]\n"
+
+        result += "[#00AAAA]chapters[/] : [\n"
+        for chapter in self.chapters:
+            chapter = str(chapter)
+            lines = chapter.split("\n")
+            for line in lines:
+                padded_line = "   " + line + "\n"
+                result += padded_line
+        result += "]\n"
+
+        return result
+
+    def print(self):
+        string_repr = self.__str__()
+        CONSOLE.print(string_repr)
+        ...
+
+# endregion classes
+
+
+# region constants
 rich_traceback_install()
 
 CHROME_DRIVER_PATH = Path("/home/jolitp/Applications/chromedriver")
@@ -33,14 +110,22 @@ CWD = Path(os.getcwd())
 DRIVER = None
 CONSOLE = Console()
 
-
+login_page = \
+"https://www.oreilly.com/member/login/?next=%2Fapi%2Fv1%2Fauth%2Fopenid%2Fauthorize%2F%3Fclient_id%3D235442%26redirect_uri%3Dhttps%3A%2F%2Flearning.oreilly.com%2Fcomplete%2Funified%2F%26state%3DpZpBIOIX0XRGL904QflVYoD4p59R7Cjm%26response_type%3Dcode%26scope%3Dopenid%2Bprofile%2Bemail&locale=en"
 # login information
-EMAIL = "libed17686@advew.com"
-PASSWORD = "h3dg3h0g"
+# registered at: 2021/07/14
+EMAIL = "vigope6498@ovooovo.com"
+PASSWORD = "H3dg3h0g"
+# endregion constants
 
+
+# region globals
 book_chapter_links = []
 book_name : str = ""
-book_folder_path = Path()
+book_folder_path = None
+book_data = Book()
+# endregion globals
+
 
 # region click_element_given_xpath(...) ================== click_element_given_xpath(...)
 def click_element_given_xpath(xpath):
@@ -77,7 +162,7 @@ def setup_driver():
 # region login_oreilly() ================================================ login_oreilly()
 def login_oreilly():
     # open site
-    DRIVER.get("https://learning.oreilly.com/")
+    DRIVER.get(login_page)
     # get email filed element and insert email
     email_field = DRIVER.find_element_by_xpath("//input[@name='email']")
     email_field.clear()
@@ -115,16 +200,22 @@ def get_book_chapter_links(page_source) -> list:
 
 # region go_to_book_page(...) ===================================== go_to_book_page(...)
 def go_to_book_page(url: str):
-    time.sleep(2)
+    time.sleep(10)
     DRIVER.get(url)
+    time.sleep(3)
 
 # TODO check if login was successful
     def check_if_login_is_successful():
         ...
     page_source = DRIVER.page_source
     global book_name
+    global book_folder_path
     book_name = DRIVER.title
-    # return None # uncomment to test w/o saving pages
+    book_data = Book()
+    book_data.title = book_name
+    book_folder_path = CWD / book_name
+
+    return None # uncomment to test w/o saving pages
 
     links_to_visit = get_book_chapter_links(page_source)
 
@@ -234,9 +325,6 @@ def save_page(url):
     page_title = page_title.replace("|", "_")
     page_title = page_title.replace(":", "_")
 
-    global book_name
-    global book_folder_path
-    book_folder_path = CWD / book_name
     if not book_folder_path.exists():
         book_folder_path.mkdir()
 
@@ -255,10 +343,89 @@ def save_page(url):
 # endregion save_page(...) ---------------------------------------------- save_page(...)
 
 
-# region get_book_data_from_files()
+# region get_chapter_names() ======================================= get_chapter_names()
+def get_chapter_names(chapter_files):
+    chapter_names = []
+    for chapter_file in chapter_files[1:]:
+        chapter_name = chapter_file.split(" _ ")[0]
+        regex = re.compile('([0-9]+_)(.*)')
+        matcher = regex.match(chapter_name)
+        if matcher:
+            chapter_name = matcher.group(2)
+        chapter_names.append(chapter_name)
+    return chapter_names
+# endregion get_chapter_names() ------------------------------------ get_chapter_names()
+
+
+# region get_chapter_sections() ================================== get_chapter_sections()
+def get_chapter_sections(chapter_files):
+    chapter_sections = []
+    # go over each file skipping the first file("main" book page)
+    for chapter_file in chapter_files[1:]:
+        with open(book_folder_path / chapter_file ) as file_obj:
+            sections = []
+            soup : BeautifulSoup = BeautifulSoup(file_obj, features="lxml")
+            section_elements = soup.find_all("section",
+                                            attrs={"data-pdf-bookmark":True})
+            for section in section_elements:
+                section = str(section)
+                pattern = re.compile('(<section data-pdf-bookmark=")(.*)(" data)')
+                matched = pattern.match(section)
+                title = matched.group(2)
+                # ic(title)
+                sections.append(title)
+            chapter_sections.append(sections)
+    return chapter_sections
+# endregion get_chapter_sections() -------------------------------- get_chapter_sections()
+
+
+# region get_chapter_hierarchy() ================================ get_chapter_hierarchy()
+def get_chapter_hierarchy():
+    # get files inside book folder
+    chapter_files = os.listdir(book_folder_path)
+    chapter_files = natsorted(chapter_files, alg=ns.PATH)
+
+    chapter_titles = get_chapter_names(chapter_files)
+    chapter_sections = get_chapter_sections(chapter_files)
+
+    for title, sections in zip(chapter_titles, chapter_sections):
+        chapter_data = Chapter()
+        chapter_data.sections = sections
+        chapter_data.title = title
+        # chapter_data.print()
+        global book_data
+        book_data.chapters.append(chapter_data)
+# endregion get_chapter_hierarchy() ============================= get_chapter_hierarchy()
+
+
+# region get_cover_picture() ======================================== get_cover_picture()
+def get_cover_picture():
+    chapter_files = os.listdir(book_folder_path)
+    chapter_files = natsorted(chapter_files, alg=ns.PATH)
+
+    main_page_file = chapter_files[0]
+    ic(main_page_file)
+
+    with open(book_folder_path / main_page_file, "r") as file_obj:
+        soup = BeautifulSoup(file_obj, features="lxml")
+        img_element = soup.find("img", attrs={"src" : True})
+        img_url = img_element["data-savepage-currentsrc"]
+        os.chdir(book_folder_path)
+        cmd = [ "wget", '-O','cover.jpeg' , img_url]
+        cmd_str = subprocess.list2cmdline(cmd)
+        ic(cmd)
+        ic(cmd_str)
+        subprocess.run(cmd)
+        os.chdir(CWD)
+
+
+# endregion get_cover_picture() ------------------------------------- get_cover_picture()
+
+
+# region get_book_data_from_files() ========================== get_book_data_from_files()
 def get_book_data_from_files():
-    # TODO get chapter hierarchy
-    # TODO get cover picture
+    # get_chapter_hierarchy()
+    get_cover_picture()
     # TODO get book info into a dict
     # TODO make org file with book info
     # TODO book title
@@ -275,8 +442,8 @@ def get_book_data_from_files():
     # TODO get errata link
 
 
-    ...
-# endregion
+    book_data.print()
+# endregion get_book_data_from_files() ---------------------- get_book_data_from_files()
 
 
 # region main() ================================================================= main()
@@ -293,5 +460,9 @@ def main():
 
 # region if __name__ == '__main__': ========================== if __name__ == '__main__':
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except Exception:
+        CONSOLE.print_exception()
+        DRIVER.quit()
 # endregion if __name__ == '__main__': ----------------------- if __name__ == '__main__':
